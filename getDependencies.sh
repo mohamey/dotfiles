a9b4bfcfa23a62ae909fdab809ee71827329ae37
= <=>
2016-08-20 14:07:16 +0100

    Automated repo addition

--- getDependencies.sh
+++ getDependencies.sh
@@ -1,13 +1,19 @@
-packages="i3 i3blocks vim compton rofi feh arc-theme zsh thunar lxappearance moka-icon-theme python2.7 git python-pip"
+packages="i3 i3blocks vim compton rofi feh arc-theme zsh thunar lxappearance moka-icon-theme python2.7 git python-pip tlp tlp-rdw playerctl"
+repos="ppa:moka/daily ppa:linrunner/tlp"
 
 # Arc Dark theme setup
 wget http://download.opensuse.org/repositories/home:Horst3180/xUbuntu_16.04/Release.key
-sudo apt-key add - < Release.key  
+sudo apt-key add - < Release.key
 sudo sh -c "echo 'deb http://download.opensuse.org/repositories/home:/Horst3180/xUbuntu_16.04/ /' >> /etc/apt/sources.list.d/arc-theme.list"
 
-# Moka icon theme
-sudo add-apt-repository ppa:moka/daily
+# Add Repositories
+for repo in $repos; do
+    echo "Adding repo $repo"
+    sudo add-apt-repository $repo
+    echo "Done"
+done
 
+# Update package list
 sudo apt-get update
 
 # Install apt-get dependencies
