1a7948bd9a5e5192fefd24ad810e9bc088806d54
Yasir Mohamed <mohamey@tcd.ie>
2016-09-22 17:58:02 -0400

    Cleaned up dotfiles for server use

--- getDependencies.sh
+++ getDependencies.sh
@@ -1,12 +1,4 @@
-packages="i3 i3blocks vim compton rofi feh arc-theme zsh thunar lxappearance moka-icon-theme python2.7 git python-pip python3-pip neovim"
-
-# Arc Dark theme setup
-wget http://download.opensuse.org/repositories/home:Horst3180/xUbuntu_16.04/Release.key
-sudo apt-key add - < Release.key
-sudo sh -c "echo 'deb http://download.opensuse.org/repositories/home:/Horst3180/xUbuntu_16.04/ /' >> /etc/apt/sources.list.d/arc-theme.list"
-
-# Moka icon theme
-sudo add-apt-repository ppa:moka/daily
+packages="zsh python3-pip neovim"
 
 # Add neovim repo
 sudo add-apt-repository ppa:neovim-ppa/unstable
@@ -19,6 +11,10 @@ for pack in $packages; do
     sudo apt-get install $pack -y
     echo "Done"
 done
+
+# Install neovim plug
+curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 # Install python for neovim
 echo "Pip3 installing neovim"
@@ -37,6 +33,3 @@ git clone https://github.com/chriskempson/base16-shell.git ~/.config/base16-shel
 # Install zprezto
 git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
 git pull && git submodule update --init --recursive
-
-# Remove Nautilus
-sudo apt-get remove nautilus
--- install.sh
+++ install.sh
@@ -4,12 +4,10 @@
 
 ##### Variables
 dir=~/dotfiles
-dotfilesConfig=~/dotfiles/config-stuff
 configDir=~/.config
 olddir=~/dotfiles_old
 oldConfigDir=~/.oldConfigs
 files="vimrc zshrc zpreztorc vim"
-configFiles="gtk-2.0 gtk-3.0 i3 i3blocks.conf"
 
 # Create dotfiles_old in homedir
 echo "Creating $olddir for backup of any existing dotfiles in ~"
@@ -30,42 +28,12 @@ for file in $files; do
 	ln -s $dir/$file ~/.$file
 done
 
-echo "Changing to $configDir"
-cd $configDir
-echo "Done"
-
-# Make .config symlinks
-for file in $configFiles; do
-    echo "Moving existing config files to $oldConfigDir"
-    mv $file $oldConfigDir/$file
-    echo "Creating symlink to $file in .config dir"
-    ln -s $dotfilesConfig/$file $file
-done
-
 # Backup system font
 echo "Backing up fonts"
-mv ~/.fonts ~/.oldFonts
+#mv ~/.fonts ~/.oldFonts
 echo "Creating symlink for fonts"
-ln -s $dir/fonts/ ~/.fonts
+#ln -s $dir/fonts/ ~/.fonts
 echo "Done"
-
-# Add GPM Script for now playing in i3Blocks
-echo "Backing up existing script"
-mkdir -p ~/Documents/oldi3Blocks
-mv ~/Documents/i3blocks/ ~/Documents/oldi3Blocks/
-echo "Creating directory ~/Documents/i3blocks for GPM script"
-mkdir -p ~/Documents/i3blocks
-echo "Adding Script to directory"
-ln -s $dir/playing.py ~/Documents/i3blocks/
-echo "Done"
-
-# Move improved battery script
-sudo rm -rf /usr/share/i3blocks/battery
-ln -s $dir/battery /usr/share/i3blocks/
-
-# Add Wallpaper
-mkdir -p ~/Pictures/Wallpapers
-cp $dir/ksp.jpg ~/Pictures/Wallpapers/ksp.jpg
 
 # Install powerline fonts
 wget https://github.com/Lokaltog/powerline/raw/develop/font/PowerlineSymbols.otf https://github.com/Lokaltog/powerline/raw/develop/font/10-powerline-symbols.conf
@@ -73,3 +41,6 @@ mkdir -p ~/.fonts/ && mv PowerlineSymbols.otf ~/.fonts/
 fc-cache -vf ~/.fonts
 mkdir -p ~/.config/fontconfig/conf.d/ && mv 10-powerline-symbols.conf ~/.config/fontconfig/conf.d/
 
+# symlink neovim config
+sudo mkdir -p ~/.config/nvim
+sudo ln -s ~/dotfiles/vimrc ~/.config/nvim/init.vim
